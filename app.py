9f6f4ecfbda41191c32869033b91e281eab2c2f8
Dikshyant15 <109207764+Dikshyant15@users.noreply.github.com>
2024-05-12 09:15:42 +0000

    first commit

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def predict():
     email_text = request.form.get("email-content")
     ##tokenizing the email_text
     tokenized_email = tokenizer.transform([email_text])
-    ##predictions
+    ##prediction
     predictions = model.predict(tokenized_email)
     predictions = 1 if predictions ==1 else -1
     return render_template("index.html", email_text=email_text, predictions = predictions)
